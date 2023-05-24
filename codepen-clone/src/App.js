c81eb5edffba17fa3c066aa018e3acca770fb984
Saurav Jha <srvtj629@gmail.com>
2023-05-24 23:07:26 +0530

    Updated

---
+++ codepen-clone/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Home from "./components/Home";
+import DataProvider from "./context/DataProvider";
+
+
+
+function App() {
+  return (
+
+    <DataProvider>
+      <Home/>
+    </DataProvider>
+  
+  );
+}
+
+export default App;
